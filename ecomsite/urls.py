6a800fd2b78da41603a3a91deb883cc07c176fb1
oohlearn <learnooh@gmail.com>
2024-05-14 12:17:03 +0800

    finish

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", v.ProductsListView.as_view(), name="products_list"),
     path("common/", v.product_list, name="product_list_common"),
     path("<int:pk>/", v.ProductDetailView.as_view(), name="product_detail"),
-    path("common/<int:id>", v.product_detail, name="detail_common")
+    path("common/<int:id>", v.product_detail, name="detail_common"),
+    path("checkout/", v.checkout, name="checkout"),
+    path("success/", v.success_order, name="success")
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 from django.views.generic import ListView
 from django.core.paginator import Paginator
 from django.views.generic.detail import DetailView
@@ -36,3 +36,26 @@ class ProductDetailView(DetailView):
 def product_detail(request, id):
     product = Product.objects.get(id=id)
     return render(request, 'shop/product_detail.html', {'product': product})
+
+
+def checkout(request):
+    if request.method == 'POST':
+        items = request.POST.get("items", "")
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        address = request.POST.get("address", "")
+        city = request.POST.get("city", "")
+        state = request.POST.get("state", "")
+        zipcode = request.POST.get("zipcode", "")
+        total = request.POST.get("total", "")
+
+        order = Order(items=items, name=name, email=email, address=address,
+                      city=city, state=state, zipcode=zipcode, total=total)
+
+        order.save()
+
+    return render(request, 'shop/checkout.html')
+
+
+def success_order(request):
+    return render(request, 'shop/success_order.html')
