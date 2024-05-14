ec83826bdf9f9b270558440f9c9eb6cfbca9766d
oohlearn <learnooh@gmail.com>
2024-05-14 15:27:09 +0800

    add admin customisation

--- shop/admin.py
+++ shop/admin.py
@@ -3,5 +3,23 @@ from .models import Product, Order
 
 # Register your models here.
 
-admin.site.register(Product)
+
+
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    def Default_category(self, request, queryset):
+        queryset.update(category="default")
+
+    list_display = ('name', 'price', "discount_price", 'category')
+    search_fields = ('name', "category")
+    actions = ('Default_category',)
+    list_editable = ("price", "category",)
+
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
